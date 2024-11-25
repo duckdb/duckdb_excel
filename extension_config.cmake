a80882619a225ef7c379aa83f08a3130526dce07
Max Gabrielsson <max@gabrielsson.com>
2024-11-25 15:07:20 +0100

    fix wasm

--- extension_config.cmake
+++ extension_config.cmake
@@ -6,5 +6,4 @@ duckdb_extension_load(excel
     INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/src/excel/include
     LOAD_TESTS
     LINKED_LIBS "../../vcpkg_installed/wasm32-emscripten/lib/lib*.a"
-    DONT_LINK
 )
